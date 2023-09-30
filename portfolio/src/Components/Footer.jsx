4574a6d491dff96a9d33aebc4623d65bdb07e7bb
Your Name <you@example.com>
2023-09-30 23:30:26 +0530

    updatefile

--- portfolio/src/Components/Footer.jsx
+++ portfolio/src/Components/Footer.jsx
@@ -3,10 +3,6 @@ import React from "react";
 const Footer = () => {
   return (
     <div class="footer">
-      <li><a href="#">Contact</a></li>
-      <li><a href="#">Contact</a></li>
-      <li><a href="#">Contact</a></li>
-      <li><a href="#">Contact</a></li>
     </div>
   );
 };
